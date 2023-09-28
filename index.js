c908ea58234473d47a9047d4540cbca7c5fbf241
Gathukumi <gathukumidavid001@gmail.com>
2023-09-28 16:17:36 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(namesArray) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < namesArray.length; i++) {
+      const message = `Thank you, ${namesArray[i]}, for the wonderful surprise gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+  
+  function countDown(startingNumber) {
+    if (startingNumber < 0) {
+      return; 
+    }
+    
+    console.log(startingNumber);
+    countDown(startingNumber - 1); 
+  }
+  
